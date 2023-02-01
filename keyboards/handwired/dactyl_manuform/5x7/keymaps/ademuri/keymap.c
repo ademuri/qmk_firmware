1b1d0dc231148437dfaf6a4f21cad6a225e1bf54
Adam DeMuri <adam.demuri@gmail.com>
2023-01-31 20:03:51 -0700

    Update keymaps for changed names

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/ademuri/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/ademuri/keymap.c
@@ -13,17 +13,17 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x7(
-     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_6  ,         KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,KC_EQL ,  KC_HOME,
-      COPY  , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,KC_ESC ,        KC_QUOT, KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
-      PASTE , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,KC_LBRC,        KC_RBRC, KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,  KC_PGUP,
-     KC_DEL , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,  KC_PGDN,
-     KC_TAB ,KC_GRV ,KC_PGDN,KC_PGUP,                                                         KC_TAB,_______,_______,  KC_END ,
+     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_6  ,         KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,KC_EQL ,      KC_HOME,
+      COPY  , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,KC_ESC ,        KC_QUOT, KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  , KC_BACKSLASH,
+      PASTE , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,KC_LBRC,        KC_RBRC, KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,      KC_PGUP,
+     KC_DEL , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,      KC_PGDN,
+     KC_TAB ,KC_GRV ,KC_PGDN,KC_PGUP,                                                         KC_TAB,_______,_______,       KC_END,
                                      KC_LGUI,ESC_CTL,KC_LALT,        KC_BSPC, KC_SPC,KC_RCTL,
                                              KC_LSFT, FN_TAB,         FN_ENT, KC_RSFT
   ),
 
   [_FUNCTION] = LAYOUT_5x7(
-     RESET  , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,  RESET  ,
+     QK_BOOT, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,  QK_BOOT,
      _______, KC_1  , KC_2  , KC_3  , KC_4  , KC_5  , KC_6  ,         KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,KC_EQL ,  _______,
      _______,_______,_______,_______,_______, KC_Y  ,_______,        _______,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,  KC_VOLU,
      _______,_______,_______,_______, KC_M  ,_______,                        _______,_______,KC_WBAK,KC_WFWD,_______,  KC_VOLD,
